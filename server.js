4ed4f5d01a0db52ba18aaa3c2e539734c64f434f
Olorunda Victory <chidiolorunda@gmail.com>
2025-08-27 14:11:52 +0100

    Edu-manager-api

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-// const jsonServer = require("json-server");
-// const server = jsonServer.create();
-// const router = jsonServer.router("db.json");
-// const middlewares = jsonServer.defaults();
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
 
-// server.use(middlewares);
-// server.use(router);
+server.use(middlewares);
+server.use(router);
 
-// const PORT = 5000;
-// server.listen(PORT, () => {
-//   console.log(`JSON Server is running on http://localhost:${PORT}`);
-// });
+const PORT = 5000;
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on http://localhost:${PORT}`);
+});
